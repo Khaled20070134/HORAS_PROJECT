446324cb42d2df832430db5983c5e488cb071830
ayag3318 <ay.ag3318@gmail.com>
2024-08-16 21:32:02 +0300

    initialize master data

--- HORAS/MasterData.cs
+++ HORAS/MasterData.cs
@@ -25,208 +25,248 @@ namespace HORAS
 {
     public static class MasterData
     {
-        public static Contractor OwnersAndContractors = new Contractor();
-        public static Contract Contracts = new Contract();
-        public static Employee employees = new Employee();
-        public static assessment assessments = new assessment();
-        public static Interims Interim = new Interims();
-        public static Employee LoggedEmployee = new Employee();
-        public static LogClass LogActivity = new LogClass();
-        public static DateTime LoginTime = new DateTime();
-        public static CollectionModel Collections = new CollectionModel();
-        
+        public static Contractor OwnersAndContractors;
+        public static Contract Contracts;
+        public static Employee employees;
+        public static assessment assessments;
+        public static Interims Interim ;
+        public static Employee LoggedEmployee;
+        public static LogClass LogActivity;
+        public static DateTime LoginTime ;
+        public static CollectionModel Collections;
 
-        public static HorasDataSet Database = new HorasDataSet();
-        public static DataBase Con = new DataBase();
-        public static List<HorasDataSet.ContractRow> contracts = new List<HorasDataSet.ContractRow>();
-        public static List<HorasDataSet.AssessmentHeadRow> Assessments = new List<HorasDataSet.AssessmentHeadRow>();
-        public static List<HorasDataSet.AssItemsRow> AssessmentsItems = new List<HorasDataSet.AssItemsRow>();
 
-        public static Login LogForm = new Login();
-        public static Start MainForm = new Start();
-        public static PartyMain PartyForm = new PartyMain();
-        public static ContractsMain ContractsMainForm = new ContractsMain();
-        public static ContractsMain ContractsMainForm_ContractConf = new ContractsMain(WizardMode.ContractConfirmations);
-        public static ContractsMain ContractsMainForm_ExpsConf = new ContractsMain(WizardMode.ExpansesConfirmations);
-        public static AssessmentMain AssMain = new AssessmentMain();
-        public static AssessmentMain AssMainWizard = new AssessmentMain(WizardMode.AssessmentsConfirmations);
-        public static EmployeesMain EmployeesForm = new EmployeesMain();
-        public static InterimsMain InterimsMain = new InterimsMain();
-        public static InterimsMain InterimsMainWizard = new InterimsMain(WizardMode.InterimsConfirmations);
+        public static HorasDataSet Database;
+        public static DataBase Con;
+        public static List<HorasDataSet.ContractRow> contracts;
+        public static List<HorasDataSet.AssessmentHeadRow> Assessments;
+        public static List<HorasDataSet.AssItemsRow> AssessmentsItems;
 
-        public static ConnectionData ConnectionData = new ConnectionData();
+        public static Login LogForm;
+        public static Start MainForm;
+        public static PartyMain PartyForm;
+        public static ContractsMain ContractsMainForm;
+        public static ContractsMain ContractsMainForm_ContractConf;
+        public static ContractsMain ContractsMainForm_ExpsConf;
+        public static AssessmentMain AssMain;
+        public static AssessmentMain AssMainWizard;
+        public static EmployeesMain EmployeesForm;
+        public static InterimsMain InterimsMain;
+        public static InterimsMain InterimsMainWizard;
+
+        public static ConnectionData ConnectionData;
         public static string ConnectionString;
 
         public static bool DatabaseConnected = false;
-        public static void LoadMasterData()
+
+        public static void Initialize()
         {
-            try
-            {
-                //DatabaseConnected = CheckConnectionString();
-                if (!CheckConnectionString()) return;
-                SetConnectionString();
-                Contracts.RefreshList();
-                assessments.RefreshList();
-                employees.RefreshList();
-                OwnersAndContractors.RefreshList();
-                LogActivity.RefreshList();
-                Interim.RefreshList();
-                Collections.RefreshList();
+                   
+         OwnersAndContractors = new Contractor();
+         Contracts = new Contract();
+         employees = new Employee();
+         assessments = new assessment();
+         Interim = new Interims();
+         LoggedEmployee = new Employee();
+         LogActivity = new LogClass();
+         LoginTime = new DateTime();
+         Collections = new CollectionModel();
 
-                DatabaseConnected = true;
-            }
-            catch (InvalidOperationException Ex)
-            {
 
-                DatabaseConnected = false;
-                return;
-            }
-        }
+         Database = new HorasDataSet();
+         Con = new DataBase();
+         contracts = new List<HorasDataSet.ContractRow>();
+       Assessments = new List<HorasDataSet.AssessmentHeadRow>();
+         AssessmentsItems = new List<HorasDataSet.AssItemsRow>();
 
-        static bool CheckConnectionString()
+         LogForm = new Login();
+         MainForm = new Start();
+         PartyForm = new PartyMain();
+            ContractsMainForm = new ContractsMain();
+            ContractsMainForm_ContractConf = new ContractsMain(WizardMode.ContractConfirmations);
+            ContractsMainForm_ExpsConf = new ContractsMain(WizardMode.ExpansesConfirmations);
+            AssMain = new AssessmentMain();
+            AssMainWizard = new AssessmentMain(WizardMode.AssessmentsConfirmations);
+            EmployeesForm = new EmployeesMain();
+            InterimsMain = new InterimsMain();
+            InterimsMainWizard = new InterimsMain(WizardMode.InterimsConfirmations);
+
+            ConnectionData = new ConnectionData();
+         ConnectionString = string.Empty;
+
+         DatabaseConnected = false;
+    }
+
+    public static void LoadMasterData()
+    {
+        try
         {
-            if (Settings1.Default.CS == string.Empty) return false;
-            try
-            {
-                var conn = new SqlConnection(Settings1.Default.CS);
-                conn.Open();
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
-        }
+            //DatabaseConnected = CheckConnectionString();
+            if (!CheckConnectionString()) return;
+            SetConnectionString();
+            Contracts.RefreshList();
+            assessments.RefreshList();
+            employees.RefreshList();
+            OwnersAndContractors.RefreshList();
+            LogActivity.RefreshList();
+            Interim.RefreshList();
+            Collections.RefreshList();
 
-        static void SetConnectionString()
+            DatabaseConnected = true;
+        }
+        catch (InvalidOperationException Ex)
         {
-            //Settings1.Default.Reload();
 
-            try
-            {
-                Contracts.ContractTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Contracts.ExpTrAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Contracts.IExpansesAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Contracts.JExpansesAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Contracts.BGLAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Collections.CollectionAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                employees.EmployeesTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                assessments.AssessmentHeadTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                assessments.AssItemsAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                OwnersAndContractors.PartyTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Interim.InterimsHeadTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                Interim.InterimsItemsTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-                LogActivity.LogTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
-            }
-            catch (InvalidOperationException Ex)
-            {
-
-                DatabaseConnected = false;
-                return;
-            }
-            // Settings1.Default.Save();
-        }
-
-        public static bool CheckMail(string email)
-        {
-            var trimmedEmail = email.Trim();
-
-            if (trimmedEmail.EndsWith("."))
-                return false;
-            try
-            {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == trimmedEmail;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public static string NumericString(double Num)
-        {
-            return Num.ToString("N", new CultureInfo("en-US"));
-        }
-        public static Item_TYPE GetItemType(string ContractNumber, string ItemNumber)
-        {
-            int ContractID = Contracts.ContractDataTable.FirstOrDefault(X => X.Number == ContractNumber).ID;
-
-            Item_TYPE Type;
-            var Result = assessments.AssItemsAdapter.GetItemType(ContractID, ItemNumber);
-            Type = (Item_TYPE)Result[0].Item_Type;
-            return Type;
-        }
-
-        public static string GetItemTypeString(int Typeint)
-        {
-            Item_TYPE Type = (Item_TYPE)Typeint;
-            string TypeReturn = string.Empty;
-            switch (Type)
-            {
-                case Item_TYPE.Supply: TypeReturn = "توريد"; break;
-                case Item_TYPE.Implementation_Installation: TypeReturn = "تنفيذ"; break;
-                case Item_TYPE.Both: TypeReturn = "تنفيذ و توريد"; break;
-            }
-            return TypeReturn;
-        }
-
-        // Copy saved filefrom client to server
-        public static void CopyFile(string FileName, string Name)
-        {
-            File.Copy(FileName, Settings1.Default.DB_Files + @"\\" + Name + Path.GetExtension(FileName));
-        }
-
-        // Seach for file and return its path in case found
-        public static string GetFile(string FileName)
-        {
-            string FoundFile = string.Empty;
-
-            if (!Directory.Exists(Settings1.Default.DB_Files))
-                return FoundFile;
-
-            string[] Files = Directory.GetFiles(Settings1.Default.DB_Files, FileName + ".pdf");
-            if (Files.Count() > 0) FoundFile = Files[0];
-
-            return FoundFile;
-        }
-
-        public static void DeleteFile(string FileName)
-        {
-            File.Delete(FileName);
-        }
-
-        public static void OpenFile(string FilePath)
-        {
-            var P = new Process();
-            P.StartInfo = new ProcessStartInfo(FilePath) { UseShellExecute = true };
-            P.Start();
-        }
-        public static void styleGridView(DataGridView MyGrid)
-        {
-            MyGrid.AlternatingRowsDefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromHtml("#eeeff9");
-            MyGrid.BorderStyle = BorderStyle.None;
-            MyGrid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            MyGrid.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromHtml("20,25,70");
-            MyGrid.ColumnHeadersDefaultCellStyle.ForeColor = Color.WhiteSmoke;
-            MyGrid.DefaultCellStyle.SelectionBackColor = Color.ForestGreen;
-            MyGrid.EnableHeadersVisualStyles = false;   
-            MyGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader);
-            MyGrid.ForeColor = Color.Black;
-        }
-
-        public static int GetcontractNum(Form sender) {
-
-            int contractNum = 0;
-
-           //Displayallcontracts newform = new Displayallcontracts(); 
-           // newform.ShowDialog();
-
-           // //newform.Show();
-
-           // contractNum = newform.SelectedContractID;
-
-           return contractNum;
+            DatabaseConnected = false;
+            return;
         }
     }
+
+    static bool CheckConnectionString()
+    {
+        if (Settings1.Default.CS == string.Empty) return false;
+        try
+        {
+            var conn = new SqlConnection(Settings1.Default.CS);
+            conn.Open();
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    static void SetConnectionString()
+    {
+        //Settings1.Default.Reload();
+
+        try
+        {
+            Contracts.ContractTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Contracts.ExpTrAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Contracts.IExpansesAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Contracts.JExpansesAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Contracts.BGLAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Collections.CollectionAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            employees.EmployeesTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            assessments.AssessmentHeadTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            assessments.AssItemsAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            OwnersAndContractors.PartyTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Interim.InterimsHeadTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            Interim.InterimsItemsTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+            LogActivity.LogTableAdapter.Connection.ConnectionString = Settings1.Default.CS;
+        }
+        catch (InvalidOperationException Ex)
+        {
+
+            DatabaseConnected = false;
+            return;
+        }
+        // Settings1.Default.Save();
+    }
+
+    public static bool CheckMail(string email)
+    {
+        var trimmedEmail = email.Trim();
+
+        if (trimmedEmail.EndsWith("."))
+            return false;
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == trimmedEmail;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static string NumericString(double Num)
+    {
+        return Num.ToString("N", new CultureInfo("en-US"));
+    }
+    public static Item_TYPE GetItemType(string ContractNumber, string ItemNumber)
+    {
+        int ContractID = Contracts.ContractDataTable.FirstOrDefault(X => X.Number == ContractNumber).ID;
+
+        Item_TYPE Type;
+        var Result = assessments.AssItemsAdapter.GetItemType(ContractID, ItemNumber);
+        Type = (Item_TYPE)Result[0].Item_Type;
+        return Type;
+    }
+
+    public static string GetItemTypeString(int Typeint)
+    {
+        Item_TYPE Type = (Item_TYPE)Typeint;
+        string TypeReturn = string.Empty;
+        switch (Type)
+        {
+            case Item_TYPE.Supply: TypeReturn = "توريد"; break;
+            case Item_TYPE.Implementation_Installation: TypeReturn = "تنفيذ"; break;
+            case Item_TYPE.Both: TypeReturn = "تنفيذ و توريد"; break;
+        }
+        return TypeReturn;
+    }
+
+    // Copy saved filefrom client to server
+    public static void CopyFile(string FileName, string Name)
+    {
+        File.Copy(FileName, Settings1.Default.DB_Files + @"\\" + Name + Path.GetExtension(FileName));
+    }
+
+    // Seach for file and return its path in case found
+    public static string GetFile(string FileName)
+    {
+        string FoundFile = string.Empty;
+
+        if (!Directory.Exists(Settings1.Default.DB_Files))
+            return FoundFile;
+
+        string[] Files = Directory.GetFiles(Settings1.Default.DB_Files, FileName + ".pdf");
+        if (Files.Count() > 0) FoundFile = Files[0];
+
+        return FoundFile;
+    }
+
+    public static void DeleteFile(string FileName)
+    {
+        File.Delete(FileName);
+    }
+
+    public static void OpenFile(string FilePath)
+    {
+        var P = new Process();
+        P.StartInfo = new ProcessStartInfo(FilePath) { UseShellExecute = true };
+        P.Start();
+    }
+    public static void styleGridView(DataGridView MyGrid)
+    {
+        MyGrid.AlternatingRowsDefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromHtml("#eeeff9");
+        MyGrid.BorderStyle = BorderStyle.None;
+        MyGrid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+        MyGrid.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromHtml("20,25,70");
+        MyGrid.ColumnHeadersDefaultCellStyle.ForeColor = Color.WhiteSmoke;
+        MyGrid.DefaultCellStyle.SelectionBackColor = Color.ForestGreen;
+        MyGrid.EnableHeadersVisualStyles = false;
+        MyGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader);
+        MyGrid.ForeColor = Color.Black;
+    }
+
+    public static int GetcontractNum(Form sender)
+    {
+
+        int contractNum = 0;
+
+        //Displayallcontracts newform = new Displayallcontracts(); 
+        // newform.ShowDialog();
+
+        // //newform.Show();
+
+        // contractNum = newform.SelectedContractID;
+
+        return contractNum;
+    }
+}
 }
--- HORAS/Program.cs
+++ HORAS/Program.cs
@@ -15,10 +15,14 @@ namespace HORAS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+             MasterData.Initialize();
             MasterData.LoadMasterData();
             if (MasterData.DatabaseConnected) Application.Run(MasterData.LogForm);
             else
                 Application.Run(MasterData.ConnectionData);
+            // MasterData.Initialize();
+            // Application.Run(new ConnectionData());
         }
     }
 }
